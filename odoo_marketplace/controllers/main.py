e07c49583b1244e4391db892b877b8a7507c7e07
AbidHussain <sahil.tus>
2022-12-26 12:12:32 +0530

    MOD[base_user_role,odoo_marketplace] changes in odoo market place module

--- odoo_marketplace/controllers/main.py
+++ odoo_marketplace/controllers/main.py
@@ -182,7 +182,7 @@ class website_marketplace_dashboard(http.Controller):
             # internal_group = request.env.ref('base.group_user')
             portal_group = request.env.ref('base.group_portal')
             if portal_group:
-                portal_group.sudo().write({"users": [(3, current_user.id, 0)]})
+                portal_group.sudo().write({"users": [(4, current_user.id, 0)]})
                 # internal_group.sudo().write({"users": [(4, current_user.id, 0)]})
             draft_seller_group_id = request.env['ir.model.data'].sudo().check_object_reference('odoo_marketplace', 'marketplace_draft_seller_group')[1]
             groups_obj = request.env["res.groups"].browse(draft_seller_group_id)
